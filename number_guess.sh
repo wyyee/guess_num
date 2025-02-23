1a9f96f91da20874b2f2adb443961dad4ad52756
wyyee wye <imhshuvo@gmail.com>
2025-02-23 13:16:52 +0000

    feat: full project

--- number_guess.sh
+++ number_guess.sh
@@ -71,3 +71,14 @@ do
   fi
 
 done
+
+# loop ends when guess is correct so, update guess
+((GUESS_COUNT++))
+
+# get user id
+USER_ID_RESULT=$($PSQL "SELECT user_id FROM players WHERE username='$USERNAME'")
+# add result to game history/database
+INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, secret_number, number_of_guesses) VALUES ($USER_ID_RESULT, $SECRET_NUMBER, $GUESS_COUNT)")
+
+# winning message
+echo You guessed it in $GUESS_COUNT tries. The secret number was $SECRET_NUMBER. Nice job\!
